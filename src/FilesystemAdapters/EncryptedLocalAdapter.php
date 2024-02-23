1eaee7006b8727a96868e73e3c5135e674c1409b
ogrygorash <ogrygorash@uvento.com>
2024-02-22 17:23:05 -0800

    Prevent attaching multiple encryption markers.

--- src/FilesystemAdapters/EncryptedLocalAdapter.php
+++ src/FilesystemAdapters/EncryptedLocalAdapter.php
@@ -219,7 +219,10 @@ class EncryptedLocalAdapter extends Local
      */
     protected function attachEncryptionMarkers($destPath)
     {
-        return $destPath . self::FILENAME_POSTFIX;
+        if (!str_ends_with($destPath, self::FILENAME_POSTFIX)) {
+            return $destPath . self::FILENAME_POSTFIX;
+        }
+        return $destPath;
     }
 
     /**
